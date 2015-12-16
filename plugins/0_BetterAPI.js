b6cce772abd7ac0a15c019566073983f03f4f146
Bluscream <admin@timo.de.vc>
2015-12-16 17:09:53 +0100

    moah commit please!

--- plugins/0_BetterAPI.js
+++ plugins/0_BetterAPI.js
@@ -2,131 +2,12 @@
 function BetterAPI() {}
 
 BetterAPI.prototype.load = function() {
-	
-	var debug = 1;
-	
-	//appendTo
-	BetterAPI.appendTo = function(link, Element){
-		var $head = $("head");
-		var $headlinklast = $head.find( link + ":last");
-		if ($headlinklast.length){
-		   $headlinklast.after(Element);
-		}
-		else {
-		   $head.append(Element);
-		};
-	};
-	
-	BetterAPI.appendTo("link[rel='stylesheet']", '<link rel="stylesheet" href="https://cdn.rawgit.com/VersatilityWerks/jAlert/master/src/jAlert-v3.css" type="text/css">');
-	// $("head").append('<script src="https://cdn.rawgit.com/hydrabolt/discord.js/master/lib/index.js"></script>');
-	// $("head").append('<script src="https://cdn.rawgit.com/izy521/discord.io/master/lib/index.js"></script>');
-	// $("head").append('<script src="https://cdn.rawgit.com/qeled/discordie/master/lib/index.js"></script>');
-	
-	//isNumber
-	BetterAPI.isNumber = function(string) {
-		return /^\d+$/.test(string);
-	};
-	//log
-	BetterAPI.log = function(dbg, type, pluginName, msg, vars) {
-		if ( (dbg = "debug") || (dbg = 1) ) {
-			if (debug == 1) {
-				switch(type) {
-					case "info":
-						console.info("[BetterDiscord] " + pluginName + ": " + msg, vars);
-						break;
-					case "warn":
-						console.warn("[BetterDiscord] " + pluginName + ": " + msg, vars);
-						break;
-					case "error":
-						console.error("[BetterDiscord] " + pluginName + ": " + msg, vars);
-						break;
-					default:
-						console.log("[BetterDiscord] " + pluginName + ": " + msg, vars);
-				};
-			} else {
-				return;
-			}
-		} else {
-			switch(type) {
-				case "info":
-					console.info("[BetterDiscord] " + pluginName + ": " + msg, vars);
-					break;
-				case "warn":
-					console.warn("[BetterDiscord] " + pluginName + ": " + msg, vars);
-					break;
-				case "error":
-					console.error("[BetterDiscord] " + pluginName + ": " + msg, vars);
-					break;
-				default:
-					console.log("[BetterDiscord] " + pluginName + ": " + msg, vars);
-			};
-		};	
-	};
-	//getUserIdByName
-	BetterAPI.getUserIdByName = function(name) {
-		var nick = "";
-		var match = "";		
-		var users = $(".member-username");
-		// console.clear();
-		for(var i = 0 ; i < users.length ; i++) {
-			var user = $(users[i]);
-			if(user.text() == name) {
-				var avatarUrl = user.closest(".member").find(".avatar-small").css("background-image");
-				match = avatarUrl.match(/\d+/);
-				nick = user.text();
-			};
-		};
-		if (match == "") {
-            [].slice.call($('.message-group')).forEach(function (message) {
-				var user = $(message).find(".user-name");
-				var username = user.text();
-				var comment = $(user).parents(".comment");
-				var silbling = $(comment).prev();
-				var avatarUrl = $(silbling).css("background-image");
-				if (name == username) {
-					match = avatarUrl.match(/\d+/);
-					nick = user.text();
-				};
-			});
-		};
-		if ( ( match != "" ) && ( /^\d+$/.test(match) ) && ( ( match.length < 17 ) || ( match.length > 18 ) ) ) {
-			console.log("BetterDiscord: "+ BetterAPI.prototype.getName() +": UID of \""+nick+"\" is "+match);
-			return match;
-		} else {
-			return null;
-		};
-	};
-	//getUserNameById
-	BetterAPI.getUserNameById = function(id) {
-		var match = "";
-		var users = $(".avatar-small");
-		for(var i = 0 ; i < users.length ; i++) {
-			var user = $(users[i]);
-			var url = user.css("background-image");
-			if(id == url.match(/\d+/)) {
-				match = user.parent().find(".member-username").text();
-			}
-		}
-		if (match == "") {
-			var users = $(".avatar-large");
-			for(var i = 0 ; i < users.length ; i++) {
-				var user = $(users[i]);
-				var url = user.css("background-image");
-				if(id == url.match(/\d+/)) {
-					match = user.parent().find(".member-username").text();
-				}
-			}
-		};
-		if (match != "") {
-			return match;
-		} else {
-			return null;
-		};
-	};
-	
-	$("head").append('<script src="https://raw.githubusercontent.com/VersatilityWerks/jAlert/master/src/jAlert-v3.min.js"></script>');
-	$("head").append('<script src="https://raw.githubusercontent.com/VersatilityWerks/jAlert/master/src/jAlert-functions.min.js"></script>');
-
+	debug = 0;
+	BetterAPI.prototype.loadCore();
+	BetterAPI.prototype.injectCSS();
+	BetterAPI.prototype.injectJS();
+	BetterAPI.prototype.loadCore();
+	BetterAPI.prototype.loadAPI();
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " loaded.");
 };
 
@@ -160,4 +41,136 @@ BetterAPI.prototype.getVersion = function() {
 
 BetterAPI.prototype.getAuthor = function() {
 	return "Bluscream";
+};
+
+BetterAPI.prototype.loadCore  = function() {
+	
+	//BetterAPI.isNumber("string");
+	BetterAPI.isNumber = function(string) {
+		return /^\d+$/.test(string);
+	};
+	
+	//BetterAPI.log(dbg, "type", "pluginName", "msg");
+	BetterAPI.log = function(dbg, type, pluginName, msg) {
+		if ( (dbg == "debug") || (dbg == "dbg") || (dbg == 1) ) {
+			if (debug == 1) {
+				switch(type) {
+					case "info":
+						console.info("[BetterDiscord] " + pluginName + ": " + msg);
+						break;
+					case "warn":
+						console.warn("[BetterDiscord] " + pluginName + ": " + msg);
+						break;
+					case "error":
+						console.error("[BetterDiscord] " + pluginName + ": " + msg);
+						break;
+					default:
+						console.log("[BetterDiscord] " + pluginName + ": " + msg);
+				};
+			} else {
+				return;
+			}
+		} else {
+			switch(type) {
+				case "info":
+					console.info("[BetterDiscord] " + pluginName + ": " + msg);
+					break;
+				case "warn":
+					console.warn("[BetterDiscord] " + pluginName + ": " + msg);
+					break;
+				case "error":
+					console.error("[BetterDiscord] " + pluginName + ": " + msg);
+					break;
+				default:
+					console.log("[BetterDiscord] " + pluginName + ": " + msg);
+			};
+		};	
+	};
+	
+	//BetterAPI.appendTo("link", "Element");
+	BetterAPI.appendTo = function(link, Element){
+		var $head = $("head");
+		var $headlinklast = $head.find( link + ":last");
+		if ($headlinklast.length){
+		   $headlinklast.after(Element);
+		}
+		else {
+		   $head.append(Element);
+		};
+	};
+};
+
+BetterAPI.prototype.injectCSS  = function() {
+	BetterAPI.appendTo("link[rel='stylesheet']", '<link rel="stylesheet" href="https://cdn.rawgit.com/VersatilityWerks/jAlert/master/src/jAlert-v3.css" type="text/css">');
+};
+
+BetterAPI.prototype.injectJS  = function() {
+	$("head").append('<script src="https://raw.githubusercontent.com/VersatilityWerks/jAlert/master/src/jAlert-v3.min.js"></script>');
+	$("head").append('<script src="https://raw.githubusercontent.com/VersatilityWerks/jAlert/master/src/jAlert-functions.min.js"></script>');
+ // $("head").append('<script src="https://cdn.rawgit.com/hydrabolt/discord.js/master/lib/index.js"></script>');
+ // $("head").append('<script src="https://cdn.rawgit.com/izy521/discord.io/master/lib/index.js"></script>');
+ // $("head").append('<script src="https://cdn.rawgit.com/qeled/discordie/master/lib/index.js"></script>');
+};
+
+BetterAPI.prototype.loadAPI  = function() {
+	// BetterAPI.getUserIdByName("name");
+	BetterAPI.getUserIdByName = function(name) {
+		var nick = "";
+		var match = "";		
+		var users = $(".member-username");
+		for(var i = 0 ; i < users.length ; i++) {
+			var user = $(users[i]);
+			if(user.text() == name) {
+				var avatarUrl = user.closest(".member").find(".avatar-small").css("background-image");
+				match = avatarUrl.match(/\d+/);
+				nick = user.text();
+			};
+		};
+		if (match == "") {
+            [].slice.call($('.message-group')).forEach(function (message) {
+				var user = $(message).find(".user-name");
+				var username = user.text();
+				var comment = $(user).parents(".comment");
+				var silbling = $(comment).prev();
+				var avatarUrl = $(silbling).css("background-image");
+				if (name == username) {
+					match = avatarUrl.match(/\d+/);
+					nick = user.text();
+				};
+			});
+		};
+		if ( ( match != "" ) && ( /^\d+$/.test(match) ) && ( ( match.length < 17 ) || ( match.length > 18 ) ) ) {
+			BetterAPI.log(1, "log", BetterAPI.prototype.getName(), "UID of \""+nick+"\" is \""+match+"\"");
+			return match;
+		} else {
+			return null;
+		};
+	};
+	// BetterAPI.getUserNameById("id");
+	BetterAPI.getUserNameById = function(id) {
+		var match = "";
+		var users = $(".avatar-small");
+		for(var i = 0 ; i < users.length ; i++) {
+			var user = $(users[i]);
+			var url = user.css("background-image");
+			if(id == url.match(/\d+/)) {
+				match = user.parent().find(".member-username").text();
+			}
+		}
+		if (match == "") {
+			var users = $(".avatar-large");
+			for(var i = 0 ; i < users.length ; i++) {
+				var user = $(users[i]);
+				var url = user.css("background-image");
+				if(id == url.match(/\d+/)) {
+					match = user.parent().find(".member-username").text();
+				}
+			}
+		};
+		if (match != "") {
+			return match;
+		} else {
+			return null;
+		};
+	};
 };
--- plugins/1_userinfo.js
+++ plugins/1_userinfo.js
@@ -1,15 +1,11 @@
 //META{"name":"userInfo"}*// Needs https://github.com/Bluscream/BetterDiscord-Plugins-and-Themes/blob/master/plugins/0_BetterAPI.js to work properly!
-//```
 function userInfo() {}
-
 userInfo.prototype.load = function() {
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " loaded.");
 };
-
 userInfo.prototype.unload = function() {
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " unloaded.");
 };
-
 userInfo.prototype.start = function() {
     $('span[data-reactid=".0.4"]').on('DOMNodeInserted', '.popout', function() {
         if ($('#UserInfo').length <= 0) {
@@ -18,42 +14,41 @@ userInfo.prototype.start = function() {
             $('#UserInfo').on("click", function () {
 				var username = $(".user-popout").find(".username").text();
 				var id = BetterAPI.getUserIdByName(username);
-				// window.prompt("Name", username);
-				// window.prompt("ID", userid);
-				infoAlert("\""+ username +"\"s Info","ID: " + id);
-				console.log("Name: \"%s\" ID: " + id, username);
+				infoAlert(username +'\'s Info','Name: '+username+'\n'+'UID: '+id);
+				// $.jAlert({
+					// 'title': username +'\'s Info',
+					// [ {'content': 'Name: '+username}, {'content': '\nUID: '+id} ]
+					// 'theme': 'blue',
+					// 'size': 'md',
+					// 'showAnimation': 'fadeInUp',
+					// 'hideAnimation': 'fadeOutDown' 
+				// });
+				console.clear();
+				BetterAPI.log(0, "info", userInfo.prototype.getName()+": "+username+'\'s Info', "\n\nName: \""+username+"\"\nUID: \""+id+"\"");
             });
         }
     });
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " started.");
 };
-
 userInfo.prototype.stop = function() {
 	$('span[data-reactid=".0.4"').off('DOMNodeInserted.userInfo');
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " stopped.");
 };
-
 userInfo.prototype.update = function() {
 	console.log("BetterDiscord: " + this.getName() + " v" + this.getVersion() + " by " + this.getAuthor() + " updated.");
 };
-
 userInfo.prototype.getName = function() {
 	return "User Info Plugin";
 };
-
 userInfo.prototype.getDescription = function() {
 	return "Adds functionality to see more information.";
 };
-
 userInfo.prototype.getVersion = function() {
 	return "1.0";
 };
-
 userInfo.prototype.getAuthor = function() {
 	return "Bluscream";
 };
-//```
-
 /*
 <div class="avatar-small" style="background-image:url(&quot;https://cdn.discordapp.com/avatars/82988905682440192/abaa0fc14a1a2940c7c1a0d21f0feb0b.jpg&quot;);" data-reactid=".0.1.1.0.2.1.1.0.0.1:$82988905682440192.0"><div class="status online" data-reactid=".0.1.1.0.2.1.1.0.0.1:$82988905682440192.0.0"></div></div>
 */
