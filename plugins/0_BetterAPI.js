9de26ca93c5d2526792c98098e32e38c589bb3eb
Bluscream <admin@timo.de.vc>
2015-12-21 22:36:34 +0100

    added settings tab and fixed user info

--- plugins/0_BetterAPI.js
+++ plugins/0_BetterAPI.js
@@ -180,7 +180,8 @@ BetterAPI.prototype.loadAPI  = function() {
 	// BetterAPI.addUserLabel("divID", "label", "<html>");
 	BetterAPI.addUserLabel = function(divID, label, html) {
 		console.log("Length: \""+$(divID).length+"\"");
-		if ($(divID.startsWith("#") ? divID : "#" + divID).length <= 0) {$('.user-popout-options').prepend(''+
+		var divID = divID.startsWith("#") ? divID.substring(1) : divID;
+        if ($("#" + divID).length <= 0) {$('.user-popout-options').prepend(''+
 			'<div id="'+divID+'"class="roles-container">'+
 				'<span class="label">'+label+'</span>'+
 				'<ul class="member-roles">'+
@@ -188,17 +189,171 @@ BetterAPI.prototype.loadAPI  = function() {
 				'</ul>'+
 			'</div>');
 		}
+		$(divID).length = 1;
 	};
 	//```
 	// BetterAPI.addUserButton("btn", "divID", "text");
 	BetterAPI.addUserButton = function(type, divID, text) {
         if ($(divID).length <= 0) {
 			$('.user-popout-options').append('<button class="'+type+'" id="'+divID+'">'+text+'</button>');
-		} else {
-			// $(divID).remove();
-			// BetterAPI.addUserButton(type, divID, text);
-		};
+		}
 	};
+	// BetterAPI.addSettingsTab("btn", "divID", "text");
+	BetterAPI.addSettingsTab = function(type, divID, text) {
+		SkypeEmos.settingsButton = null;
+		SkypeEmos.settingsPanel = null;
+		SkypeEmos.prototype.settings_changeTab = function(tab) {
+
+			SkypeEmos.settings_lastTab = tab;
+
+			var controlGroups = $("#se-control-groups");
+			$(".se-tab").removeClass("selected");
+			$(".se-pane").hide();
+			$("#" + tab).addClass("selected");
+			$("#" + tab.replace("tab", "pane")).show();
+
+			switch (tab) {
+				case "se-settings-tab":
+					break;
+			}
+		};
+
+		SkypeEmos.settings_updateSetting = function(checkbox) {
+			var cb = $(checkbox).children().find('input[type="checkbox"]');
+			var enabled = !cb.is(":checked");
+			var id = cb.attr("id");
+			cb.prop("checked", enabled);
+			SkypeEmos.settings[id] = enabled;
+			SkypeEmos.prototype.saveSettings()
+			console.log("id", enabled)
+		}
+
+		SkypeEmos.settings_construct = function() {
+
+			SkypeEmos.settingsPanel = $("<div/>", {
+				id: "se-pane",
+				class: "settings-inner",
+				css: {
+					"display": "none"
+				}
+			});
+
+			var settingsInner = '' +
+				'<div class="scroller-wrap">' +
+				'   <div class="scroller settings-wrapper settings-panel">' +
+				'       <div class="tab-bar TOP">' +
+				'           <div class="tab-bar-item se-tab" id="se-settings-tab" onclick="SkypeEmos.prototype.settings_changeTab(\'se-settings-tab\');">Settings</div>' +
+				'       </div>' +
+				'       <div class="se-settings">' +
+				'' +
+				'               <div class="se-pane control-group" id="se-settings-pane" style="display:none;">' +
+				'                   <ul class="checkbox-group">';
+
+			for (var setting in SkypeEmos.settingsArray) {
+				var sett = SkypeEmos.settingsArray[setting];
+				var id = sett["id"];
+				if (sett["implemented"]) {
+					settingsInner += '' +
+						'<li>' +
+						'<div class="checkbox" onclick="SkypeEmos.settings_updateSetting(this);" >' +
+						'<div class="checkbox-inner">' +
+						'<input type="checkbox" id="' + id + '" ' + (SkypeEmos.settings[id] ? "checked" : "") + '>' +
+						'<span></span>' +
+						'</div>' +
+						'<span>' + setting + " - " + sett["info"] +
+						'</span>' +
+						'</div>' +
+						'</li>';
+				}
+			}
+			settingsInner += '</ul>' +
+				'               </div>' +
+				' </div>' +
+				' </div>' +
+				'</div>'
+
+			function show_Settings() {
+				$(".tab-bar-item").removeClass("selected");
+				SkypeEmos.settingsButton.addClass("selected");
+				$(".form .settings-right .settings-inner").hide();
+
+				SkypeEmos.settingsPanel.show();
+				if (SkypeEmos.settings_lastTab == "") {
+					SkypeEmos.prototype.settings_changeTab("se-settings-tab");
+				} else {
+					SkypeEmos.prototype.settings_changeTab(SkypeEmos.settings_lastTab);
+				}
+			}
+
+			SkypeEmos.settingsButton = $("<div/>", {
+				class: "tab-bar-item",
+				text: "Skype-Emos",
+				id: "se-settings-new",
+				click: function() {
+					setTimeout(show_Settings, 100);
+				}
+			});
+
+			SkypeEmos.settingsPanel.html(settingsInner);
+
+			function defer() {
+				if ($(".btn.btn-settings").length < 1) {
+					setTimeout(defer, 100);
+				} else {
+					$(".btn.btn-settings").first().on("click", function() {
+
+						function innerDefer() {
+							if ($(".modal-inner").first().is(":visible")) {
+
+								SkypeEmos.settingsPanel.hide();
+								var tabBar = $(".tab-bar.SIDE").first();
+
+								$(".tab-bar.SIDE .tab-bar-item:not(#bd-settings-new)").click(function() {
+									$(".form .settings-right .settings-inner").first().show();
+									$("#se-settings-new").removeClass("selected");
+									SkypeEmos.settingsPanel.hide();
+								});
+								$(".tab-bar.SIDE .tab-bar-item#bd-settings-new").click(function() {
+									$("#se-settings-new").removeClass("selected");
+									SkypeEmos.settingsPanel.hide();
+								});
+
+								tabBar.append(SkypeEmos.settingsButton);
+								$(".form .settings-right .settings-inner").last().after(SkypeEmos.settingsPanel)
+								$("#se-settings-new").removeClass("selected");
+							} else {
+								setTimeout(innerDefer, 100);
+							}
+						}
+						innerDefer();
+					});
+				}
+			}
+			defer();
+		};
+		SkypeEmos.settingsArray = {
+		"Enable Skype Emotes in Messages": {
+			"id": "emote-enable",
+			"info": "Enable Message Parsing",
+			"default": true,
+			"implemented": true
+		},
+		"Show Skype Emo list": {
+			"id": "emote-list",
+			"info": "Shows the Skype Emo list",
+			"default": true,
+			"implemented": false
+		},
+		"Show Emo Names": {
+			"id": "emote-tooltip",
+			"info": "Shows the Emo Names",
+			"default": true,
+			"implemented": false
+		},
+		
+	};
+}
+	
 };
 BetterAPI.prototype.loadEvents  = function() {
 	$('span[data-reactid=".0.4"]').on('DOMNodeInserted', '.popout', function() {
@@ -213,3 +368,5 @@ BetterAPI.prototype.unloadEvents  = function() {
 	$('span[data-reactid=".0.4"]').off('DOMNodeInserted');
 	$('body').off('DOMSubtreeModified');
 };
+
+
