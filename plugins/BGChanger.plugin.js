2264ef3ea2af8e1a369cd26990bd8b6d11d76538
Bluscream <admin@timo.de.vc>
2016-01-25 07:51:28 +0100

    hmm

--- plugins/BGChanger.plugin.js
+++ plugins/BGChanger.plugin.js
@@ -23,11 +23,12 @@ BGChanger.prototype.getSettingsPanel = function() {
 };
 BGChanger.prototype.load = function() {
 	BG_original = $('.app').css('background-image');
+	BGchange = true;
+	BGrand = false;
 };
 BGChanger.prototype.unload = function() {
 };
 BGChanger.prototype.start = function() {
-	BGchange = true;
 	BGChanger.changeBG();
 };
 BGChanger.prototype.stop = function() {
@@ -39,36 +40,41 @@ BGChanger.prototype.onSwitch = function() {
 };
 BGChanger.changeBG = function() {
 	if (BGchange == true) {
-		bgarray = [ // Remember that the images need to be HTTPS encrypted!
-			'https://i.imgur.com/emSvYMy.gif',
-			'https://i.imgur.com/RktuAes.gif',
-			'https://i.imgur.com/AFWa9a4.gif',
-			'https://i.imgur.com/9mv2XGt.gif',
-			'https://i.imgur.com/cHDC1Nu.gif',
-			'https://i.imgur.com/upaf5C3.gif',
-			'https://i.imgur.com/MlfDS2G.gif',
-			'https://i.imgur.com/Sk8uISa.gif',
-			'https://i.imgur.com/mJBEies.gif',
-			'https://i.imgur.com/W2OFM8r.gif',
-			'https://i.imgur.com/ENhKvZD.gif',
-			'https://i.imgur.com/NJa2qMN.gif',
-			'https://i.imgur.com/HTyT6I7.gif',
-			'https://i.imgur.com/UMGJwOh.gif',
-			'https://i.imgur.com/EAS8sXK.gif',
-			'https://i.imgur.com/LFSjvzJ.gif',
-			'https://i.imgur.com/gmuhE5e.gif',
-			'https://i.imgur.com/HufwA6q.gif',
-			'https://i.imgur.com/BEwkp5Q.gif',
-			'https://i.imgur.com/d0EfsaT.gif',
-			'https://i.imgur.com/Yy2qNQe.gif',
-			'https://i.imgur.com/PteVvRN.gif',
-			'https://i.imgur.com/Nv6afLv.gif',
-			'https://i.imgur.com/PPg7DIn.gif',
-			'https://i.imgur.com/1QYu9wS.gif',
-			'https://i.imgur.com/Hd1kXIa.gif'
-		];
-		var rand = bgarray[Math.floor(Math.random() * bgarray.length)];
-		$('.app').css('background-image', 'url('+rand+')');
-		rand = null;
+		if (BGrand == true){
+			bgarray = [ // Remember that the images need to be HTTPS encrypted!
+				'https://i.imgur.com/emSvYMy.gif',
+				'https://i.imgur.com/RktuAes.gif',
+				'https://i.imgur.com/AFWa9a4.gif',
+				'https://i.imgur.com/9mv2XGt.gif',
+				'https://i.imgur.com/cHDC1Nu.gif',
+				'https://i.imgur.com/upaf5C3.gif',
+				'https://i.imgur.com/MlfDS2G.gif',
+				'https://i.imgur.com/Sk8uISa.gif',
+				'https://i.imgur.com/mJBEies.gif',
+				'https://i.imgur.com/W2OFM8r.gif',
+				'https://i.imgur.com/ENhKvZD.gif',
+				'https://i.imgur.com/NJa2qMN.gif',
+				'https://i.imgur.com/HTyT6I7.gif',
+				'https://i.imgur.com/UMGJwOh.gif',
+				'https://i.imgur.com/EAS8sXK.gif',
+				'https://i.imgur.com/LFSjvzJ.gif',
+				'https://i.imgur.com/gmuhE5e.gif',
+				'https://i.imgur.com/HufwA6q.gif',
+				'https://i.imgur.com/BEwkp5Q.gif',
+				'https://i.imgur.com/d0EfsaT.gif',
+				'https://i.imgur.com/Yy2qNQe.gif',
+				'https://i.imgur.com/PteVvRN.gif',
+				'https://i.imgur.com/Nv6afLv.gif',
+				'https://i.imgur.com/PPg7DIn.gif',
+				'https://i.imgur.com/1QYu9wS.gif',
+				'https://i.imgur.com/Hd1kXIa.gif'
+			];
+			result = bgarray[Math.floor(Math.random() * bgarray.length)];
+		} else {
+			result = 'https://i.imgur.com/cHDC1Nu.gif';
+		}
+		$('.platform-win').append('<div id="bgimage"></div>')
+		$('#bgimage').css('background-image', 'url('+result+')');
+		result = null;
 	}
 };
