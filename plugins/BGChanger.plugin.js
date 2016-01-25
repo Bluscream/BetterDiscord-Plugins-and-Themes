1b59b68a608dec61e19670cbd17e8c59de0a9e8a
Bluscream <admin@timo.de.vc>
2016-01-25 08:45:35 +0100

    a lot shit :o

--- plugins/BGChanger.plugin.js
+++ plugins/BGChanger.plugin.js
@@ -24,7 +24,8 @@ BGChanger.prototype.getSettingsPanel = function() {
 BGChanger.prototype.load = function() {
 	BG_original = $('.app').css('background-image');
 	BGchange = true;
-	BGrand = false;
+	BGrand = true;	BGimg = 'https://i.imgur.com/MlfDS2G.gif';
+	BGblur = false;
 };
 BGChanger.prototype.unload = function() {
 };
@@ -33,7 +34,11 @@ BGChanger.prototype.start = function() {
 };
 BGChanger.prototype.stop = function() {
 	BGchange = false;
-	$('.app').css('background-image', BG_original);
+	if (BGblur == true){
+		$('#bgimage').css('background-image', BG_original);
+	}else{
+		$('.app').css('background-image', BG_original);
+	}
 };
 BGChanger.prototype.onSwitch = function() {
 	// BGChanger.changeBG();
@@ -71,10 +76,37 @@ BGChanger.changeBG = function() {
 			];
 			result = bgarray[Math.floor(Math.random() * bgarray.length)];
 		} else {
-			result = 'https://i.imgur.com/cHDC1Nu.gif';
+			result = BGimg;
 		}
-		$('.platform-win').append('<div id="bgimage"></div>')
-		$('#bgimage').css('background-image', 'url('+result+')');
+		if (BGblur == true){
+			$('.platform-win').append('<div id="bgimage"></div>')
+			$('#bgimage').attr('style', '');
+			$('#bgimage').css('background-image', 'url('+result+') !important');
+			$('#bgimage').css('position', 'fixed !important');
+			$('#bgimage').css('left', '0 !important');
+			$('#bgimage').css('right', '0 !important');
+			$('#bgimage').css('z-index', '1 !important');
+			$('#bgimage').css('display', 'block !important');
+			$('#bgimage').css('width', '100% !important');
+			$('#bgimage').css('height', '100% !important');
+			$('#bgimage').css('background-size', 'cover !important');
+			$('#bgimage').css('-webkit-filter', 'blur(3px) !important');
+			
+			$('.app').attr('style', "test");
+			$('.app').css('position', 'fixed !important');
+			$('.app').css('left', '0 !important');
+			$('.app').css('right', '0 !important');
+			$('.app').css('z-index', '1 !important');
+			$('.app').css('margin-left', '20px !important');
+			$('.app').css('margin-right', '20px !important');
+		}else{
+			$('.app').attr('style', 'background:transparent !important;background-size:cover !important;background-image:url('+result+') !important');
+			// $('.app').attr('style', "test");
+			// $('.app').css('background', 'transparent !important');
+			// $('.app').css('background-image', 'url('+result+') !important');
+			// $('.app').css('background-size', 'cover !important');
+		}
+		console.log('Changed background to '+result);
 		result = null;
 	}
 };
