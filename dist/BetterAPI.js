d17cd0b5cbbe32e5980600deabf49ac7aa058704
Bluscream <admin@timo.de.vc>
2016-04-25 19:15:12 +0200

    11

--- dist/BetterAPI.js
+++ dist/BetterAPI.js
@@ -30,45 +30,45 @@
 		return this.split(search).join(replacement);
 	};
 //	'String'.quote();
-	// if(!String.prototype.quote){
-	  // String.prototype.quote = (function(){
-		// var escp_regex = /[\\"]/g,
-		  // escp_callback = '\\$&',
-		  // ctrl_map = {
-			// '\b': '\\b', // backspace
-			// '\t': '\\t', // tab
-			// '\n': '\\n', // new line
-			// '\f': '\\f', // form feed
-			// '\r': '\\r'  // carriage return
-		  // },
-		  // ctrl_regex = new RegExp('[\b\t\n\f\r]', 'g'),
-		  // ctrl_callback = function(match){
-			// return ctrl_map[match];
-		  // },
-		  // xhex_regex = /[\x00-\x07\x0B\x0E-\x1F\x7F-\xFF]/g,
-		  // xhex_callback = function(match, char_code){
-			// char_code = match.charCodeAt(0);
-			// return '\\x' + (char_code < 16 ? '0' : '') + char_code;
-		  // },
-		  // uhex_regex = /[\u0100-\uFFFF]/g,
-		  // uhex_callback = function(match, char_code){
-			// char_code = match.charCodeAt(0);
-			// return '\\u' + (char_code < 4096 ? '0' : '') + char_code;
-		  // },
-		  // stringify = typeof JSON !== 'undefined' && JSON.stringify;
-		// return function(){
-		  // var self = this; // promote compression
-		  // if(self == null) throw new TypeError('can\'t convert ' + self + ' to object');
-		  // if(stringify) return stringify(self);
-		  // return '"' + self
-			// .replace(escp_regex, escp_callback)
-			// .replace(ctrl_regex, ctrl_callback)
-			// .replace(xhex_regex, xhex_callback)
-			// .replace(uhex_regex, uhex_callback) + '"';
-		// }
-	  // }());
-	  // String.quote = Function.call.bind(''.quote);
-	// }
+	if(!String.prototype.quote){
+	  String.prototype.quote = (function(){
+		var escp_regex = /[\\"]/g,
+		  escp_callback = '\\$&',
+		  ctrl_map = {
+			'\b': '\\b', // backspace
+			'\t': '\\t', // tab
+			'\n': '\\n', // new line
+			'\f': '\\f', // form feed
+			'\r': '\\r'  // carriage return
+		  },
+		  ctrl_regex = new RegExp('[\b\t\n\f\r]', 'g'),
+		  ctrl_callback = function(match){
+			return ctrl_map[match];
+		  },
+		  xhex_regex = /[\x00-\x07\x0B\x0E-\x1F\x7F-\xFF]/g,
+		  xhex_callback = function(match, char_code){
+			char_code = match.charCodeAt(0);
+			return '\\x' + (char_code < 16 ? '0' : '') + char_code;
+		  },
+		  uhex_regex = /[\u0100-\uFFFF]/g,
+		  uhex_callback = function(match, char_code){
+			char_code = match.charCodeAt(0);
+			return '\\u' + (char_code < 4096 ? '0' : '') + char_code;
+		  },
+		  stringify = typeof JSON !== 'undefined' && JSON.stringify;
+		return function(){
+		  var self = this; // promote compression
+		  if(self == null) throw new TypeError('can\'t convert ' + self + ' to object');
+		  if(stringify) return stringify(self);
+		  return '"' + self
+			.replace(escp_regex, escp_callback)
+			.replace(ctrl_regex, ctrl_callback)
+			.replace(xhex_regex, xhex_callback)
+			.replace(uhex_regex, uhex_callback) + '"';
+		}
+	  }());
+	  String.quote = Function.call.bind(''.quote);
+	}
 //	load();
 	load = function() {
 		window.location.href="https://discordapp.com/channels/@me";
